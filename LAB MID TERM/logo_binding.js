8b9dc570c8d921bddf0cd056554907172b6bffb2
HamnaKhawar <72753447+HamnaKhawar@users.noreply.github.com>
2023-04-17 13:57:59 +0500

    FA20-bcs-092

---
+++ LAB MID TERM/logo_binding.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // JavaScript code for logo click event
+    document.getElementById('logo').addEventListener('click', function() {
+      var rollNumber = 'FA20-BCS-092'; 
+      alert('Roll Number: ' + rollNumber);
+    });
+  });
+  
